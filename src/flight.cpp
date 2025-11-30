1c8663762e75f37af16b0957b9d634a80ce13706
Jacob Plourde <jacob_plourde@hotmail.com>
2025-11-29 19:33:58 -0700

    Add required functions for Flight (coding for Route still needed)

---
+++ src/flight.cpp
@@ -0,0 +1,34 @@
+#include "flight.h"
+
+Flight::Flight(int num_rows, int num_seats_per_row, Route& route): number_of_rows(num_rows), number_of_seats_per_row(num_seats_per_row), {
+  this->route = new Route(route.get_source(), route.get_destination());
+  populate_seats_vector();
+}
+
+void Flight::populate_seats_vector() {
+  seats.resize(number_of_rows);
+  for(int i = 0; i < number_of_seats_per_row; i++) {
+    seats.at(i).resize(number_of_seats_per_row);
+  }
+
+  for(int i = 0; i < number_of_rows; i++) {
+    for(int j = 0; j < number_of_seats_per_row; j++) {
+      seats.at(i).at(j).set_row_number(i);
+      seats.at(i).at(j).set_seat_character('A' + j);
+    }
+  }
+}
+
+void Flight::addPassenger(int id, string& fname, string& lname, string& phone, int row_num, char seat_char) {
+  passengers.push_back(Passenger(id, fname, lname, phone, &seats.at(row_num).at(seat_char - 'A'))); 
+  seats.at(row_num).at(seat_char - 'A').set_occupied(true);
+}
+
+void Flight::removePassenger(int id) {
+  for(int i = 0; i < passengers.size(); i++) {
+    Passenger *cur = &passengers.at(i);
+    if(cur->get_id() == id) {
+      cur->get_seat()->set_occupied(false);
+    }
+  }
+}
--- src/flight.h
+++ src/flight.h
@@ -14,9 +14,16 @@ private:
   vector<Passenger> passengers;
   int number_of_rows;
   int number_of_seats_per_row;
-  Route route;
-  void populate_seats();
-  void addPassenger(string& fname, string &lname, string& phone, int row_num, char seat_char);
+  Route *route;
+  void populate_seats_vector();
+public:
+  Flight(int num_rows, int num_seats_per_row, Route& route);
+  void addPassenger(int id, string& fname, string &lname, string& phone, int row_num, char seat_char);
+  void removePassenger(int id);
+  void print_seat_map();
+  int get_number_of_rows()const { return number_of_rows; };
+  int get_number_of_seats_per_row()const { return number_of_seats_per_row; };
+
 };
 
 #endif
