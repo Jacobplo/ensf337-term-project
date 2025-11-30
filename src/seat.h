0658a41670b4571fbdb69a189ec2a3e22574b19c
Jacob Plourde <jacob_plourde@hotmail.com>
2025-11-29 19:33:09 -0700

    Add getters and setters to Seat.

--- src/seat.h
+++ src/seat.h
@@ -11,7 +11,11 @@ private:
 public:
   Seat(int row_num, char seat_char);
   int get_row_number() const { return row_number; };
+  void set_row_number(int num) { row_number = num; };
   char get_seat_character() const { return seat_character; };
+  void set_seat_character(char chr) { seat_character = chr; };
+  bool get_occupied()const { return occupied; };
+  void set_occupied(bool state) { occupied = state; };
 };
 
 #endif
