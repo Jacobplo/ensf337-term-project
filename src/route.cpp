6843de6fc43c6b36664ac585380dc5db8e1f1145
UnasKhalid <96748553+UnasKhalid@users.noreply.github.com>
2025-11-30 23:46:58 -0700

    route.cpp source code

--- src/route.cpp
+++ src/route.cpp
@@ -1,10 +1,22 @@
 /*
- *  File Name: route.cpp
- *  Assignment: Term Project
- *  Lecture Section: L01
- *  Completed by: Jacob Plourde
- *  Development Date: November 29, 2025
- */
+ *  File Name: route.cpp
+ *  Assignment: ENSF 337 Term Project
+ *  Lab and Lecture Section: 1
+ *  Completed by: Unas Khalid
+ *  Development Date: November 28, 2025
+ */
+
 #include "route.h"
 
-Route::Route(string& src, string& dest): source(src), destination(dest) {};
+Route::Route(const string &from, const string &to) {
+    origin = from;
+    destination_city = to;
+}
+
+string Route::get_source() const {
+    return origin;
+}
+
+string Route::get_destination() const {
+    return destination_city;
+}
