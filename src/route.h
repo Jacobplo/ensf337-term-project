b6811abf93dab67693e0dafc2b85c5ad2305d0de
JuliaIzuogu <juliaizuogu@gmail.com>
2025-11-30 23:45:54 -0700

    Update route.h

--- src/route.h
+++ src/route.h
@@ -5,6 +5,14 @@
  *  Completed by: Jacob Plourde
  *  Development Date: November 29, 2025
  */
+//
+//  route.h
+//  Project 1
+//
+//  Created by Julia Izuogu on 2025-11-30.
+//
+
+
 #ifndef ROUTE_H
 #define ROUTE_H
 
@@ -18,9 +26,9 @@ private:
   string destination;
 public:
   Route() {};
-  Route(string& src, string& dest);
-  string get_source()const { return source; };
-  string get_destination()const { return destination; };
+  Route(const string& src, const string& dest);
+  string get_source()const { return source; }
+  string get_destination()const { return destination; }
 };
 
 #endif
